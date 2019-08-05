8bad9350f329f83f261dcb3bb655ffbf4bc94e9e
Brandon Fries <brandonfries1993@gmail.com>
2019-08-05 18:21:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,10 @@ def display_rainbow(colors)
   'viloet']
   puts "R: #{colors[0]}"
   puts "O: #{color[1]}"
+  puts "Y: #{colors[2]}"
+  puts "G: #{colors[3]}"
+  puts "B: #{colors[4]}"
+  puts "I: #{colors[5]}"
+  puts "V: #{colors[6]}"
 
 end
